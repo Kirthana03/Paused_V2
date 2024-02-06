5baf8a86423c2eacc62e157c49e928911f20224b
Kirthana03 <131602714+Kirthana03@users.noreply.github.com>
2024-02-06 12:49:05 +0530

    AnimChanges

--- opacity-toggle.js
+++ opacity-toggle.js
@@ -1,10 +1,16 @@
 AFRAME.registerComponent('opacity-toggle',{
     init: function() {
         let el = this.el;
+        let varPos = el.getAttribute('position'); 
+        let varPosX = varPos['x']; 
+        let varPosY = varPos['y'] + 20; 
+        let varPosZ = varPos['z'];
+
         this.toggleState = function() {
             console.log('clicked');
             // el.setAttribute('position','0 2 -60'); 
-            el.setAttribute('animation', 'property: scale; from:1 1 1; to: 0 0 0; dur:2000; loop:false');
+            // el.setAttribute('animation', 'property: scale; from:1 1 1; to: 0 0 0; dur:2000; loop:false');
+            el.setAttribute('animation', `property: position; to: 0 ${varPosY} ${varPosZ}; dur:2000; loop:false`);
             // el.components.sound.stopSound();
             // st2.components.sound.playSound();
         }
